c7af8c9a07c35511fae90b5c433f4ce39b760aa0
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-04-13 12:27:40 -0600

    worked on game logic

---
+++ game.js
@@ -0,0 +1,37 @@
+const lives = document.querySelector("#lives")
+const loseLifeButton = document.querySelector("")
+const livesDiv = document.querySelector("")
+var livesArray =[]
+var counter = 5
+
+
+initializePage()
+
+loseLifeButton.addEventListener("click", loseHeart)
+
+if (livesArray.length != counter) {
+  livesDiv.removeChild(list.childNode[0])
+}
+
+function initializePage(){
+  for (var i = 0; i < 5; i++) {
+    let img = createNode(img)
+
+    img.src =
+
+    append(lives, img)
+  }
+
+}
+
+function createNode(element){
+  return document.createElement(element)
+}
+
+function append(parent, element){
+  return parent.appendChild(element)
+}
+function loseHeart(){
+  counter -= 1
+  return counter
+}
